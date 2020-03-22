9b76d2920f8faa2eadd76c772674d1c556fa99b5
Yvette Smith <yvette@boesman.nl>
2020-03-22 21:25:12 +0000

    test travis issue

--- blog/settings.py
+++ blog/settings.py
@@ -83,6 +83,7 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 if "DATABASE_URL" in os.environ:
+    print("Postgres URL found: " + os.environ.get('DATABASE_URL'))
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
