831b1882c630a9756fe19866d329097de310d01c
Yvette Smith <yvette@boesman.nl>
2020-03-22 21:32:11 +0000

    Delete accidentally committed ignored git file

--- env.py
+++
@@ -1,4 +0,0 @@
-import os
-os.environ.setdefault("SECRET_KEY", '7$d-jm-4-xbq13hapbwmfe7qhqchn_%wcdmwes_sekmhw-w5aj')
-os.environ.setdefault('DATABASE_URL', 
-"postgres://bwqrmsxclcrtbd:bcd4fe8b741e98bfa419904048cd5576dfefc1ef4ca87345e6fd0be8f14f3bcb@ec2-54-247-169-129.eu-west-1.compute.amazonaws.com:5432/d4ovmu57b8v9ru")
